6a38d17f6dc0d49fe65221b88f798dfdce126fd4
Anja Antolkovic <anja.antolkovic@gmail.com>
2023-06-01 17:13:46 +0200

    api calls

--- server.py
+++ server.py
@@ -1,10 +1,25 @@
 # Module: Data representation
 # Author: Anja Antolkovic
 
-from flask import Flask
+from flask import Flask, jsonify
+
 
 
 app = Flask(__name__, static_url_path='', static_folder='static_pages')
+
+
+isins = [{'isin_code' : 'XS1906456220', 
+          'issuance_date' : '09/11/2018',
+          'maturity_date' : '09/11/2061',
+          'issuer_name' : 'International Bank for Reconstruction and Development', 
+          'coupon_rate' : 0,
+          'denomination' : 'EUR'},
+          {'isin_code' : 'XS1833442426', 
+          'issuance_date' : '07/06/2018',
+          'maturity_date' : '06/06/2024',
+          'issuer_name' : 'FMO-Ned.Fin.-Maat.is v.Ontw.NV', 
+          'coupon_rate' : 0,
+          'denomination' : 'USD'}]
 
 
 @app.route('/')
@@ -14,17 +29,29 @@ def hello():
 # get all
 @app.route('/isins')
 def get_all(): 
-    return 'served by get_all()'
+    return jsonify(isins)
 
 # get isin
 @app.route('/isins/<isin_code>')
-def get_isin_code(isin_code): 
-    return 'served by get_isin_code()' + ' ' + str(isin_code)
-
+def get_isin_code(isin_code):
+    found_isins = list(filter(lambda t : t['isin_code'] == isin_code, isins))
+    if len(found_isins) == 0:
+        return jsonify({}), 204
+    return jsonify(found_isins[0]) 
+    
 # create isin
 @app.route('/isins', methods = ['POST'])
 def create_isin(): 
-    return 'served by create_isin()'
+    new_isin = {
+          'isin_code' : 'xxx', 
+          'issuance_date' : 'ccc',
+          'maturity_date' : 'ttt',
+          'issuer_name' : 'eee', 
+          'coupon_rate' : 0,
+          'denomination' : 'EUR'
+           }
+    isins.append(new_isin)
+    return jsonify(new_isin)
 
 # update isin
 @app.route('/isins/<isin_code>', methods = ['PUT'])
@@ -38,4 +65,6 @@ def delete_isin(isin_code):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True)
+
+
