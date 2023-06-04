4d261a965e002714d4990f731fe4b849adce16b0
Anja Antolkovic <anja.antolkovic@gmail.com>
2023-06-04 14:33:00 +0200

    created table

---
+++ create_table.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '',
+    database = 'collateral'
+)
+
+cursor = db.cursor()
+sql = '''create table ea_assets(
+        isin_code varchar(20), 
+        issuance_date varchar(20), 
+        maturity_date varchar(20), 
+        issuer_name varchar(100), coupon_rate int, 
+        denomination varchar(100), 
+        primary key(isin_code));'''
+
+cursor.execute(sql)
+
+db.commit()
+db.close()
+cursor.close()
