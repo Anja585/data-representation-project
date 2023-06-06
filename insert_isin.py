1bff23bf6da500cbb8cd79798756f805b17e681a
Anja Antolkovic <anja.antolkovic@gmail.com>
2023-06-06 15:36:11 +0200

    syntax

--- insert_isin.py
+++ insert_isin.py
@@ -9,20 +9,16 @@ db = mysql.connector.connect(
 )
 
 cursor = db.cursor()
-sql = '''insert into ea_assets (index,
-                                isin_code, 
+sql = '''insert into ea_assets (isin_code, 
                                 issuance_date, 
                                 maturity_date, 
                                 coupon_rate, 
-                                denomination) values (%s%s%s%s%s%s)
+                                denomination) values (%s,%s,%s,%s,%s);
                                 '''
-values = (2222222, 'DE000A289DC1', '13/02/2022', '13/02/2060', '5', 'EUR')
+values = ('CCCCC1', '13/02/2022', '13/02/2060', 5, 'EUR')
 
 cursor.execute(sql, values)
-cursor.commit()
-result = cursor.fetchall()
-for x in result:
-    print(x) 
+db.commit()
 
 db.close()
 cursor.close()    
